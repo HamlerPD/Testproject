2bc4a7dd98178ef166f19d1953b1d99ecd7b9432
hamle <hamlersd6@gmail.com>
2024-09-11 20:05:33 -0400

    interpolacion de variables

--- Testproject/OperandoTF.cs
+++ Testproject/OperandoTF.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Testproject
 {
@@ -43,24 +44,145 @@ namespace Testproject
 
             Console.WriteLine(S1 != S2);//False*/
 
-            Random dice = new Random();
+            /* Random dice = new Random();
 
-            int roll1 = dice.Next(1, 100);
+             int roll1 = dice.Next(1, 100);
 
 
-            if (roll1 >= 50)
+             if (roll1 >= 50)
+             {
+                 Console.WriteLine("Head");
+
+
+             }else
+             {
+                 Console.WriteLine("Tails");
+             }
+
+             Random coin = new Random();
+             int flip = coin.Next(0, 2);
+             Console.WriteLine((flip == 0) ? "heads" : "tails");*/
+
+            /*string permission = "Admin|Manager";
+            int level = 55;
+
+            if(level >= 55)
             {
-                Console.WriteLine("Head");
+                Console.WriteLine("Welcome, Super Admin user");
 
-
-            }else
+            }else if (level <= 55)
             {
-                Console.WriteLine("Tails");
+                Console.WriteLine("Welcome, Admin user ");
+
+            }else if(level > 20)
+            {
+                Console.WriteLine("Contact an Admin for access");
+
+            }else if(level < 20)
+            {
+                Console.WriteLine("You do not have sufficient privileges.");
+
+            }else 
+            { Console.WriteLine("You do not have sufficient privileges.");
+
+            }*/
+            /* string permission = "Admin|Manager";
+             int level = 53;
+
+             if (permission.Contains("Admin"))
+             {
+                 if (level > 55)
+                 {
+                     Console.WriteLine("Welcome, Super Admin user.");
+                 }
+                 else
+                 {
+                     Console.WriteLine("Welcome, Admin user.");
+                 }
+             }
+             else if (permission.Contains("Manager"))
+             {
+                 if (level >= 20)
+                 {
+                     Console.WriteLine("Contact an Admin for access.");
+                 }
+                 else
+                 {
+                     Console.WriteLine("You do not have sufficient privileges.");
+                 }
+             }
+             else
+             {
+                 Console.WriteLine("You do not have sufficient privileges.");
+             }
+
+             int value = 5 >= 10 ? 10 : 20;
+             Console.WriteLine($"{value}");*/
+
+            /* bool flag = true;
+             int value = 10;
+             if (flag)
+             {
+
+                 Console.WriteLine($"inside the code block: {value}");
+             }
+
+             value = 20;
+             Console.WriteLine($"inside the code block: {value}");*/
+
+            /* int value = 5;
+             if (value >= 0)
+             {
+                 int value2 = 6;
+                 int result = value + value2;
+
+                 Console.WriteLine($"el resultado es igual: { result}");
+
+             }*/
+
+            //SOLUCION MIA
+            /*int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+             int total = 0;
+             bool found = true;
+             foreach (int number in numbers)
+             {
+
+                 total += number;
+
+                 if (number == 42)
+                 {
+
+                     if (found)
+                     {
+                         Console.WriteLine("Set contains 42");
+
+                     }
+
+                 }
+
+             }
+             Console.WriteLine($"Total: {total}");*/
+
+            //SOLUCION DE LA PAGINA
+
+            /*int[] numbers = { 4, 8, 15, 16, 23, 42 };
+            int total = 0;
+            bool found = false;
+
+            foreach (int number in numbers)
+            {
+                total += number;
+                if (number == 42)
+                    found = true;
             }
 
-            Random coin = new Random();
-            int flip = coin.Next(0, 2);
-            Console.WriteLine((flip == 0) ? "heads" : "tails");
+            if (found)
+                Console.WriteLine("Set contains 42");
+
+            Console.WriteLine($"Total: {total}");*/
+
+
 
         }    
     }
